00756fb64bdc1094e8f377be9e3c0c740e22ad8f
KSB21ST <21supersoo@kaist.ac.kr>
2022-07-26 13:58:18 +0900

    add backend code

--- server/src/schema.sql
+++ server/src/schema.sql
@@ -1,13 +1,16 @@
+DROP TABLE IF EXISTS logs;
+DROP TABLE IF EXISTS testsets;
+
 CREATE TABLE logs (
     id              INTEGER         PRIMARY KEY     AUTOINCREMENT   ,
     task_id         TEXT            NOT NULL                        ,
     user_id         TEXT            NOT NULL                        ,
     action_sequence TEXT            NOT NULL
-)
+);
 
 CREATE TABLE testsets (
     id              INTEGER         PRIMARY KEY     AUTOINCREMENT   ,
     user_id         TEXT            NOT NULL                        ,
     testjson        TEXT            NOT NULL                        ,
     ratings         INTEGER         NOT NULL
-)
+);
